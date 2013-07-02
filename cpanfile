ba241c8d1ad12a49a5674bdd98e4bf6e626863f5
yagince <straitwalk@gmail.com>
2013-07-02 22:44:52 +0900

    intial

--- cpanfile
+++ cpanfile
@@ -1,1 +1,3 @@
+requires 'Mouse', 0;
 requires 'Readonly', 0;
+requires 'DBIx::Skinny', 0;
---
+++ lib/MoDo/Model/ToDo.pm
@@ -0,0 +1,21 @@
+package ToDo;
+use Mouse;
+
+has 'id' => (
+    is => 'ro',
+    isa => 'Int',
+    default => sub { -1 },
+);
+
+has 'title' => (
+    is => "rw",
+    isa => "Str",
+    default => q(),
+    required => 1,
+);
+
+has 'created_at' => (
+    is => "ro",
+    isa => "DateTime",
+    required => 1,
+);
